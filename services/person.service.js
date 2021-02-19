17259e6dc16cfd7e7d00e8be772989fdf229dbf8
Björn Yttergren <Bjorn.Yttergren@umeaenergi.se>
2021-02-19 16:34:58 +0100

    fix: hardcoded ssn

--- services/person.service.js
+++ services/person.service.js
@@ -1,6 +1,6 @@
-export async function info() {
+export async function info(pnr) {
   const coors = `https://ume-cors.herokuapp.com/`;
-  return fetch(coors + "https://www.motalaenergi.se/zavann/get_address.php?ssn=7911068513", {
+  return fetch(coors + "https://www.motalaenergi.se/zavann/get_address.php?ssn=" + pnr, {
     "headers": {
     },
     "referrer": "https://www.motalaenergi.se/privatperson/teckna-avtal",
