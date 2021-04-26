880bbf87fff581d7a49ec64fdddc4d583613a6f9
Björn Yttergren <Bjorn.Yttergren@umeaenergi.se>
2021-04-26 14:19:47 +0200

    sdsdsd

---
+++ components/showcase/DemoComponent.vue
@@ -0,0 +1,24 @@
+<template>
+  <section class="max-w-7xl mx-auto px-4 sm:px-6">
+    <h2 class="mb-8 md:text-center font-extrabold text-xl md:text-4xl lg:text-5xl">{{body.headline || 'Headline...'}}</h2>
+    <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8">
+      <div v-for="index in 10" :key="index" class="text-6xl font-black rounded-lg bg-gradient-to-br from-indigo-300 to-fuchsia-500 h-64 flex justify-center items-center">
+      <span class=" animate-bounce">{{index}}</span> 
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+import { defineComponent } from "@vue/composition-api";
+
+export default defineComponent({
+  props: {
+    body: {
+      type: Object,
+      required: true,
+    },
+  },
+  setup() {},
+});
+</script>
